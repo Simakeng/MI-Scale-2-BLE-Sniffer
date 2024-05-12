b38c8fbd4b7f27ec3ce6a63c957bf22335a7f05f
simakeng <simakeng@outlook.com>
2024-05-13 06:51:04 +0800

    Add config for wifi and udp.

---
+++ Components/UDPClient/udp_task.h
@@ -0,0 +1,14 @@
+/**
+ * @file udp_task.c
+ * @author simakeng (simakeng@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-05-13
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <esp_check.h>
+
+esp_err_t init_udp_task(void);
--- main/main.c
+++ main/main.c
@@ -2,10 +2,24 @@
 #include <freertos/task.h>
 #include <esp_log.h>
 #include <status_led.h>
+#include <udp_task.h>
+#include <esp_netif.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+
+
+#define TAG "main"
 
 void app_main()
 {
-    ESP_LOGI("main", "Device bootup!");
+    ESP_LOGI(TAG, "Device bootup!");
+
+    // Do system initialization
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_LOGI(TAG, "Initialize LED task...");
 
     init_led_task();
 
@@ -14,7 +28,11 @@ void app_main()
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-    ESP_LOGI("main", "Scan for WiFi APs...");
+    ESP_LOGI(TAG, "Initialize UDP task...");
+
+    init_udp_task();
+
+    ESP_LOGI(TAG, "Scan for WiFi APs...");
 
     led_set_color(0, 0, 255);
     led_set_mode(LED_MODE_BREATH);
